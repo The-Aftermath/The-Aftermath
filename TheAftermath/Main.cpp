e486f4ce40aa443c56a737ef8f454de836c691a9
Wxz <429373960@qq.com>
2022-08-08 23:22:51 +0800

    RenderWindow

--- TheAftermath/Main.cpp
+++ TheAftermath/Main.cpp
@@ -1,16 +1,26 @@
 #include <Windows.h>
 #include <winnt.h>
+#include <DirectXMath.h>
 
 #include <winrt/base.h>
 
 #include "Window/RenderWindow.h"
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+
+    if (!DirectX::XMVerifyCPUSupport()) {
+        return 1;
+    }
+
     winrt::init_apartment();
 
-
+    TheAftermath::RenderWindow window;
 
     winrt::clear_factory_cache();
     winrt::uninit_apartment();
--- TheAftermath/Window/RenderWindow.h
+++ TheAftermath/Window/RenderWindow.h
@@ -2,11 +2,23 @@
 #include <Windows.h>
 #include <string>
 #include <string_view>
+#include <cstdint>
 namespace TheAftermath {
 
 	class RenderWindow {
 	public:
-		RenderWindow() {}
+		RenderWindow() { mWidth = 800; mHeight = 600; }
+		RenderWindow(const std::wstring_view v, uint32_t w, uint32_t h) :mTitle(v), mWidth(w), mHeight(h) {}
+		RenderWindow(const RenderWindow&) = delete;
+		RenderWindow(RenderWindow&&) noexcept = default;
+		~RenderWindow() {}
+
+		RenderWindow& operator=(const RenderWindow&) = delete;
+		RenderWindow& operator=(RenderWindow&&) noexcept = default;
+
 	private:
+		std::wstring mTitle;
+		uint32_t mWidth;
+		uint32_t mHeight;
 	};
 }
