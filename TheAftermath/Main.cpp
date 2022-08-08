7d0abff3bedabb59de2302bc8ecb1576f3d99b55
Wxz <429373960@qq.com>
2022-08-08 18:18:19 +0800

    init com

--- TheAftermath/Main.cpp
+++ TheAftermath/Main.cpp
@@ -1,11 +1,15 @@
 #include <Windows.h>
 #include <winnt.h>
+#include <wrl/wrappers/corewrappers.h>
 
 #include "Window/RenderWindow.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-    
+    Microsoft::WRL::Wrappers::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+    if (FAILED(initialize))
+        return 1;
+
     return 0;
 }
