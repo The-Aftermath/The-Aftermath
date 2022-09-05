0eba2e5a6bf3b1d222053afc3927b163b219f165
Wxz <429373960@qq.com>
2022-09-06 05:32:05 +0800

    WndProc

--- TheAftermath/Main.cpp
+++ TheAftermath/Main.cpp
@@ -2,12 +2,7 @@
 
 #include "bgfx/bgfx.h"
 
-int WINAPI wWinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR lpCmdLine,
-    _In_ int nShowCmd
-) {
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
 
     return 0;
 }
