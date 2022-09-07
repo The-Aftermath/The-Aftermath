67c3d9501a9ee7422f27d931d66ddc52a0fb127e
Wxz <429373960@qq.com>
2022-09-08 05:25:49 +0800

    update 3rd dll

--- TheAftermath/Device.cpp
+++ TheAftermath/Device.cpp
@@ -1,30 +1,43 @@
 #include "Device.h"
 #include "bgfx/bgfx.h"
 #include <combaseapi.h>
+#include <thread>
 namespace TheAftermath {
+
+	void func() {
+		ID3D12Device7* pDevice;
+		if (SUCCEEDED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&pDevice)))) {
+			pDevice->Release();
+			OutputDebugStringW(L"sss\n");
+		}
+		else {
+			OutputDebugStringW(L"error\n");
+		}
+	}
 
 	struct ADevice : public Device {
 
 		ADevice(DeviceDesc* pDecs) {
 
-			D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_2, IID_PPV_ARGS(&pDevice));
-			bgfx::Init init;
-			init.type = bgfx::RendererType::Direct3D12;
-			init.vendorId = BGFX_PCI_ID_NONE;
-			init.platformData.nwh = pDecs->mHwnd;
-			init.platformData.ndt = nullptr;
-			//init.platformData.context = pDevice;
-			init.resolution.width = pDecs->mWidth;
-			init.resolution.height = pDecs->mHeight;
-			init.resolution.reset = BGFX_RESET_VSYNC;
-			bgfx::init(init);
+
+			D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&pDevice));
+			//bgfx::Init init;
+			//init.type = bgfx::RendererType::Direct3D12;
+			//init.vendorId = BGFX_PCI_ID_NONE;
+			//init.platformData.nwh = pDecs->mHwnd;
+			//init.resolution.width = pDecs->mWidth;
+			//init.resolution.height = pDecs->mHeight;
+			//init.resolution.reset = BGFX_RESET_VSYNC;
+			//bgfx::init(init);
+			std::thread myNewThread(func);
+			myNewThread.join();
 
 		}
 
 		~ADevice() {
 			
-			bgfx::shutdown();
-			pDevice->Release();
+			//bgfx::shutdown();
+			//pDevice->Release();
 		}
 
 		void Release() {
@@ -32,7 +45,7 @@ namespace TheAftermath {
 		}
 
 		void Present() {
-			bgfx::frame();
+			//bgfx::frame();
 		}
 
 		ID3D12Device7* GetDevice() const {
@@ -40,6 +53,8 @@ namespace TheAftermath {
 		}
 
 		ID3D12Device7* pDevice;
+		ID3D12Device7* pDevice1;
+		ID3D12Device7* pDevice2;
 	};
 
 	Device* CreateDevice(DeviceDesc* pDecs) {
--- TheAftermath/Device.h
+++ TheAftermath/Device.h
@@ -3,6 +3,7 @@
 
 #include <Windows.h>
 #include <d3d12.h>
+#include <dxgi1_6.h>
 #include <cstdint>
 namespace TheAftermath {
 
