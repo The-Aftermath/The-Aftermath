c755b18feb33a8d6335165c5de421466d7e01a96
Wxz <429373960@qq.com>
2022-09-11 11:37:37 +0800

    remove bgfx

--- TheAftermath/Graphics.cpp
+++ TheAftermath/Graphics.cpp
@@ -1,33 +1,19 @@
 #include "Graphics.h"
-#include "bgfx/c99/bgfx.h"
+
 namespace TheAftermath {
 
 
 	struct ADevice : public Device {
 
 		ADevice(DeviceDesc* pDesc) {
-			bgfx_init_t init;
-			bgfx_init_ctor(&init);
-#ifdef _DEBUG
-			init.debug = true;
-#endif
-			init.type = BGFX_RENDERER_TYPE_DIRECT3D12;
-			init.vendorId = BGFX_PCI_ID_NONE;
-			init.platformData.nwh = pDesc->mHwnd;
-			init.resolution.width = pDesc->mWidth;
-			init.resolution.height = pDesc->mHeight;
-			init.resolution.reset = BGFX_RESET_VSYNC;
 
-			bgfx_init(&init);
-			auto data = bgfx_get_internal_data();
-			pDevice = (ID3D12Device7*)data->context;
 
 			mWidth = pDesc->mWidth;
 			mHeight = pDesc->mHeight;
 		}
 
 		~ADevice() {
-			bgfx_shutdown();
+
 		}
 
 		void Release() {
@@ -46,9 +32,5 @@ namespace TheAftermath {
 
 	Device* CreateDevice(DeviceDesc* pDesc) {
 		return new ADevice(pDesc);
-	}
-
-	uint16_t LoadProgram(const char* _vsName, const char* _fsName) {
-		return 1;
 	}
 }
--- TheAftermath/Scene.cpp
+++ TheAftermath/Scene.cpp
@@ -1,5 +1,5 @@
 #include "Scene.h"
-#include "bgfx/c99/bgfx.h"
+
 namespace TheAftermath {
 	struct AScene : public Scene {
 		AScene(SceneDesc* pDesc) {
@@ -8,31 +8,6 @@ namespace TheAftermath {
 			mWidth = pDesc->mWidth;
 			mHeight = pDesc->mHeight;
 
-			mGBufferBaseColorView = 0;
-			mGBufferDepthView = 1;
-
-			const uint64_t tsFlags = 0
-				| BGFX_SAMPLER_MIN_POINT
-				| BGFX_SAMPLER_MAG_POINT
-				| BGFX_SAMPLER_MIP_POINT
-				| BGFX_SAMPLER_U_CLAMP
-				| BGFX_SAMPLER_V_CLAMP
-				;
-			bgfx_attachment_t gbufferAt[2];
-			mGBufferBaseColorHandle = bgfx_create_texture_2d(uint16_t(mWidth), uint16_t(mHeight), false, 1, BGFX_TEXTURE_FORMAT_RGBA32F, BGFX_TEXTURE_RT | tsFlags, nullptr);
-			bgfx_attachment_init(&gbufferAt[0], mGBufferBaseColorHandle, BGFX_ACCESS_WRITE, 0, 1, 0, BGFX_RESOLVE_NONE);
-
-			mGBufferDepthHandle = bgfx_create_texture_2d(uint16_t(mWidth), uint16_t(mHeight), false, 1, BGFX_TEXTURE_FORMAT_D32F, BGFX_TEXTURE_RT | tsFlags, nullptr);
-			bgfx_attachment_init(&gbufferAt[1], mGBufferDepthHandle, BGFX_ACCESS_WRITE, 0, 1, 0, BGFX_RESOLVE_NONE);
-
-			mGBufferFBOHandle = bgfx_create_frame_buffer_from_attachment(2, gbufferAt, true);
-		
-			bgfx_set_view_clear(0
-				, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH
-				, 0x00ff30ff
-				, 1.0f
-				, 0
-			);
 		}
 
 		~AScene() {
@@ -44,10 +19,7 @@ namespace TheAftermath {
 		}
 
 		void Update() {
-			bgfx_set_view_rect(0, 0, 0, uint16_t(mWidth), uint16_t(mHeight));
-			bgfx_touch(0);
 
-			bgfx_frame(false);
 		}
 
 		Device* pDevice;
@@ -55,12 +27,6 @@ namespace TheAftermath {
 		uint32_t mWidth;
 		uint32_t mHeight;
 
-		bgfx_texture_handle_t mGBufferBaseColorHandle;
-		bgfx_texture_handle_t mGBufferDepthHandle;
-		bgfx_frame_buffer_handle_t mGBufferFBOHandle;
-
-		bgfx_view_id_t mGBufferBaseColorView;
-		bgfx_view_id_t mGBufferDepthView;
 	};
 
 	Scene* CreateScene(SceneDesc* pDesc) {
