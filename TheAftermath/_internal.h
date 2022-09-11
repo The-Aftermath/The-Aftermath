e16cb65a9a6f355fe5d8f14dfd664288c79df973
Wxz <429373960@qq.com>
2022-09-11 15:33:54 +0800

    copy shader

--- TheAftermath/_internal.h
+++ TheAftermath/_internal.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "bgfx/c99/bgfx.h"
+#include <filesystem>
 namespace TheAftermath {
 	
 	inline bgfx_program_handle_t loadProgram(const char* _vsName, const char* _fsName) {
-
+		std::filesystem::path vsRPath{ _vsName }, fsRPath{ _fsName };
+		vsRPath += ".bin"; fsRPath += ".bin";
+		std::filesystem::path parentPath{ "ShaderBin" };
+		auto vsPath = parentPath / vsRPath;
+		auto fsPath = parentPath / fsRPath;
 	}
 }
