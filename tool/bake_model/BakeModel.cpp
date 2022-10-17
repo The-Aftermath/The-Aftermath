f0ccc477122d670ee656136dca24ef367ef13c3a
Wxz <429373960@qq.com>
2022-10-17 23:15:05 +0800

    add json 3rd

--- tool/bake_model/BakeModel.cpp
+++ tool/bake_model/BakeModel.cpp
@@ -1,7 +1,16 @@
-#include <assimp/Exporter.hpp>
+#include <assimp/Importer.hpp>
+#define CGLTF_IMPLEMENTATION
+#define CGLTF_WRITE_IMPLEMENTATION
+#include <cgltf_write.h>
 
-int main() {
-	Assimp::Exporter exporter;
 
+int main(int argc, char** argv) {
+	Assimp::Importer importer;
+	cgltf_options options{};
+	cgltf_data* data = NULL;
+	cgltf_result result = cgltf_parse_file(&options, "C:\\Users\\42937\\Documents\\GitHub\\glTF-Sample-Models\\2.0\\Box With Spaces\\glTF\\Box With Spaces.gltf", &data);
+
+	cgltf_free(data);
+	//cgltf_free(pddd);
 	return 0;
 }
